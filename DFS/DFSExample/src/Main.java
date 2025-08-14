cfd05ea62da7fc3ac8129e1311b5077c17cd6325
Ashish <ashishsingh@gmail.com>
2025-08-13 22:22:29 -0700

    Checking in code for DFS Recursive as well as using  a stack.

--- DFS/DFSExample/src/Main.java
+++ DFS/DFSExample/src/Main.java
@@ -42,7 +42,8 @@ public class Main {
         tree.root.right = new Node<>(3);
         tree.root.right.left = new Node<>(6);
         tree.root.right.right = new Node<>(7);
-        tree.root.right.left.right = new Node<>(10);
+
+        tree.root.right.right.left = new Node<>(10);
         tree.root.right.right.right = new Node<>(11);
         tree.root.right.right.right.right = new Node<>(12);
 
