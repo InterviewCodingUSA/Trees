5a2b6ec42f282e0f16ea374f2551c7a78bce728d
Ashish <ashishsingh@gmail.com>
2025-08-14 11:15:32 -0700

    Checking in code for Print Bottom View.

---
+++ PrintBottomView/PrintBottomView/src/BinTree.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+
+// Binary tree class that manages a tree structure of nodes
+// Uses generic type T that must be comparable for ordering
+public class BinTree<T extends Comparable<T>> {
+    // Root node of the binary tree
+    public Node<T> root;
+
+    // Default constructor initializing an empty tree
+    public BinTree() {
+        this.root = null;
+    }
+    public void printBottomView(){
+        if(root == null){
+            return;
+        }
+        BoxValue<Integer> minValue = new BoxValue<>(0);
+        BoxValue<Integer> maxValue = new BoxValue<>(0);
+
+        HashMap<Integer,Node<T>> map = new HashMap<>();
+
+        printBottomView(root, 0, map, minValue, maxValue);
+
+        for(int i = minValue.data ; i <= maxValue.data; i ++){
+            System.out.printf(map.get(i).data + ", ");
+        }
+        System.out.println();
+
+    }
+
+    private void printBottomView( Node<T> node,
+                                  Integer distance,
+                                  HashMap<Integer, Node<T>> map,
+                                  BoxValue<Integer> minValue,
+                                  BoxValue<Integer> maxValue){
+        if(node != null){
+            // adding value to the hash map
+            map.put(distance, node);
+            if(distance < minValue.data){
+                minValue.data = distance;
+            }
+            if(distance > maxValue.data){
+                maxValue.data = distance;
+            }
+            printBottomView(node.left, distance - 1, map, minValue, maxValue);
+            printBottomView(node.right, distance + 1, map, minValue, maxValue);
+
+        }
+    }
+}
