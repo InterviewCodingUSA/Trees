ff11bf7038e12bf410aac625a0ec0dbcd530e579
Ashish <ashishsingh@gmail.com>
2025-08-14 10:12:16 -0700

    Checking in code for Print right view.

---
+++ PrintRightView/PrintRightView/src/BinTree.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Binary tree class that manages a tree structure of nodes
+// Uses generic type T that must be comparable for ordering
+public class BinTree<T extends Comparable<T>> {
+    // Root node of the binary tree
+    public Node<T> root;
+
+    // Default constructor initializing an empty tree
+    public BinTree() {
+        this.root = null;
+    }
+
+    public void printRightView(){
+        if(root == null){
+            return;
+        }
+
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+
+        Node<T> prevNode = null;
+
+        while(!queue.isEmpty()){
+            Node<T> node = queue.remove();
+
+            if(node != null){
+                prevNode = node;
+                if(node.left!= null){
+                    queue.add(node.left);
+                }
+                if(node.right!= null){
+                    queue.add(node.right);
+                }
+            }
+            else{
+                // We have reached to a new level
+                System.out.println(prevNode.data);
+                if(queue.isEmpty()){
+                    break;
+                }
+                queue.add(null);
+            }
+        }
+    }
+
+}
